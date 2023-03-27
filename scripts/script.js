47918a8c9288d4dc179db490120894b0e68e66c0
Herman L <hluo21@my.bcit.ca>
2023-03-27 15:37:23 -0700

    Add multiple eventlisteners for bgcolor and change btn text - slide 7

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,19 @@ const alertUser = () => {
 
 clickMeBtn.addEventListener("click", alertUser, {once: true})
 
+
+const pinkBackground = () => {
+    document.body.classList.add("pink-bg")
+}
+
+const clickedText = () => {
+    if (clickMeBtn.textContent == "Click Me!") {
+        clickMeBtn.textContent = "clicked!"
+    }
+    else {
+        clickMeBtn.textContent = "Click Me!"
+    }
+}
+
+clickMeBtn.addEventListener("click", pinkBackground)
+clickMeBtn.addEventListener("click", clickedText)
